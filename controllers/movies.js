5ebe484b3a20849f6b8673b9ce7b23624b00bd62
Phyllis Wong <wong.phyllism@gmail.com>
2018-09-06 12:48:53 -0700

    users can now see a list of movies

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const MovieDb = require('moviedb-promise');
+const moviedb = new MovieDb('abd49a1bba895c620b6e924e4f3a0098');
 
 app.get('/', (req, res) => {
-  res.render('movies/movies-index');
+  moviedb.miscNowPlayingMovies().then(response => {
+    res.render('movies/movies-index', { movies: response.results });
+  }).catch(console.error)
 });
 
 module.exports = app;
